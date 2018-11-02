9a7728526e0628428ec5a0191390e5a5bfe9d0f0
karinsk <karinskyg@gmail.com>
2018-11-02 16:15:38 +0200

    a

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ def contract_func():
     parser.add_argument(dest="str_in")
     args = parser.parse_args()
     str_in = args.str_in
-    str_input = str(str_in)
-
-    dict_hist = {}
-    lst = [i for i in str_input]
-    for i in lst:
-        if i in dict_hist:
-            dict_hist[i]+=1
+    #str_input = str(str_in)
+    count = 1
+    last_num_seq = False
+    curr_num = str_in[0]
+    for i in range(1, len(str_in)):
+        if str_in[i-1] == str_in[i]:
+            count+=1
         else:
-            dict_hist[i]=1
-    for key_hist in dict_hist.keys():
-        print(key_hist+str(dict_hist[key_hist]), end='', flush=True)
+            last_num_seq = True
+        if last_num_seq or i == len(str_in)-1:
+            print(curr_num+str(count), end='', flush=True)
+            curr_num = str_in[i]
+            count = 1
+            last_num_seq = False
 
 
 if __name__ == "__main__":
